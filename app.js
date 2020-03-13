8510b02aef0175423ae386d456a7a2eea3959fe0
Toni Galata <batscott@gmail.com>
2020-03-12 20:21:40 -0600

    added js complete

--- app.js
+++ app.js
@@ -1,42 +1,59 @@
 let choices = ['rock', 'paper', 'scissors']
+
 let userChoice = ""
 let compChoice = ""
 let userScore = 0
 let compScore = 0
 
 var rock = document.getElementById('rock') // rock = 1
-    rock.addEventListener("click", setChoice(0))  
+rock.addEventListener("click", function (){setChoice(0);});
+
 var paper = document.getElementById('paper') // paper = 2
-    paper.addEventListener("click", setChoice(1)) 
+paper.addEventListener("click", function (){setChoice(1);}); 
+
 var scissors = document.getElementById('scissors') // sciss = 3
-    scissors.addEventListener("click", setChoice(2))
+scissors.addEventListener("click", function (){setChoice(2);});
 
 function setChoice(num) {
 userChoice = choices[num]
-compChoice = choices[Math.round(Math.random() * 3)]
+compChoice = choices[Math.round(Math.random() * 2)]
 showDown(userChoice, compChoice)
+console.log(`Your choice: ${userChoice} Computer choice: ${compChoice}`)
 }
 
 function showDown(userChoice, compChoice) {
-  if (userChoice === compChoice){userTie}
-  else if(userChoice === 1 && compChoice === 3){userWin}
-  else if(userChoice === 2 && compChoice === 1){userWin}
-  else if(userChoice === 3 && compChoice === 2){userWin}
-  else {userLose}
+  if (userChoice === compChoice){
+    userTie()
+    console.log("TIE")
+  }
+  else if(userChoice === 'rock' && compChoice === 'scissors'){
+    userWin()
+    console.log("WIN")
+  }
+  else if(userChoice === 'paper' && compChoice === 'rock'){
+    userWin()
+    console.log("WIN")
+  }
+  else if(userChoice === 'scissors' && compChoice === 'paper'){
+    userWin()
+    console.log("WIN")
+  }
+  else {
+    userLose()
+    console.log("LOSE")
+  }
 }
-
-function userTie{
+function userTie(){
   document.getElementById("score").innerHTML = 'YOU TIE!!!!'
 }
-function userWin{
+function userWin(userScore, compScore){
   userScore += 1
   document.getElementById("score").innerHTML = 'YOU WIN!!!'
 }
-function userLose{
+function userLose(userScore, compScore){
   compScore += 1
   document.getElementById("score").innerHTML = 'YOU LOSE!!!'
 }
-
-function showScores{
+function showScores(userScore, compScore){
   document.getElementById("totals").innerHTML = `Your score: ${userScore} Computer score: ${compScore}`
 }
